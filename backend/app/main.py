e23b6ec6e584f84b7642887aa9dc45ec87d6f177
Marcelo Giovani Chies Filho <m4rc3l0chies@gmail.com>
2026-07-17 18:24:49 -0300

    feat: add a fronted to the project

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from sqlalchemy import text
 
@@ -12,6 +13,17 @@ from app.database.models import WebhookEndpoint
 app = FastAPI(
     title="HookScope",
     version="0.1.0",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+    ],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(deliveries_router)
